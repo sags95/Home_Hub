8743c437cde50e1045dfeaee5a854c62300effde
Alex Sagel <alex.sagel7587@gmail.com>
2020-03-18 12:38:56 -0400

    more API work

--- lib/clockBase.dart
+++ lib/clockBase.dart
@@ -1,12 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:homehub/Widgets/digital_clock.dart';
 import 'keys.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 class ClockBase extends StatelessWidget {
-
+  //final NetworkImage backgroundPic = null;
 
   @override
   Widget build(BuildContext context) {
+
+    Future getUnsplashPic() async {
+      String url = 'https://api.unsplash.com/photos/random/?client_id=' + unsplashAPI;
+      http.Response response = await http.get(url);
+      if (response.statusCode == 200){
+        var decodedData = jsonDecode(response.body);
+        print(decodedData[0]['id']);
+      }
+    }
+
+    getUnsplashPic();
+
     return Container(
       decoration: BoxDecoration(
         image: DecorationImage(
